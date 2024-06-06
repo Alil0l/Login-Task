1deb98558911bb4d6e19f531e880cc35df2d678d
Ali Mostafa <aliwrker011@gmail.com>
2024-06-06 21:17:04 +0300

    modify links

--- script/index.js
+++ script/index.js
@@ -7,10 +7,10 @@ var popUp = document.querySelector(".popup");
 var isUnique = false;
 var usersData = [];
 var validationStates = {
-    name: false,
-    email: false,
-    password: false,
-    phone: false,
+  name: false,
+  email: false,
+  password: false,
+  phone: false,
 };
 var useName = /^[a-zA-Z0-9_ -]{3,15}$/;
 var usePass = /^[a-zA-Z0-9_ -^]{8,20}$/;
@@ -18,20 +18,27 @@ var useEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 var usePhone = /^(?:\+20|0)?1[0125]\d{8}$/;
 // Validating functions
 function validator(input, reg, type) {
-    input.addEventListener("input", function () {
-        var _a, _b, _c, _d;
-        var temp = input.value;
-        if (temp.length < 3 || !reg.test(temp)) {
-            (_a = input.parentElement) === null || _a === void 0 ? void 0 : _a.classList.add("wrong-input");
-            (_b = input.parentElement) === null || _b === void 0 ? void 0 : _b.classList.remove("correct-input");
-            validationStates[type] = false;
-        }
-        else {
-            (_c = input.parentElement) === null || _c === void 0 ? void 0 : _c.classList.remove("wrong-input");
-            (_d = input.parentElement) === null || _d === void 0 ? void 0 : _d.classList.add("correct-input");
-            validationStates[type] = true;
-        }
-    });
+  input.addEventListener("input", function () {
+    var _a, _b, _c, _d;
+    var temp = input.value;
+    if (temp.length < 3 || !reg.test(temp)) {
+      (_a = input.parentElement) === null || _a === void 0
+        ? void 0
+        : _a.classList.add("wrong-input");
+      (_b = input.parentElement) === null || _b === void 0
+        ? void 0
+        : _b.classList.remove("correct-input");
+      validationStates[type] = false;
+    } else {
+      (_c = input.parentElement) === null || _c === void 0
+        ? void 0
+        : _c.classList.remove("wrong-input");
+      (_d = input.parentElement) === null || _d === void 0
+        ? void 0
+        : _d.classList.add("correct-input");
+      validationStates[type] = true;
+    }
+  });
 }
 // Initializing validators
 validator(userName, useName, "name");
@@ -39,77 +46,90 @@ validator(userPassword, usePass, "password");
 validator(userPhone, usePhone, "phone");
 validator(userEmail, useEmail, "email");
 function validateUniqueEmail() {
-    if (userEmail.value != "" &&
-        !usersData.some(function (e) { return e.email === userEmail.value; })) {
-        isUnique = true;
-    }
-    return isUnique;
+  if (
+    userEmail.value != "" &&
+    !usersData.some(function (e) {
+      return e.email === userEmail.value;
+    })
+  ) {
+    isUnique = true;
+  }
+  return isUnique;
 }
 // Signup button actions
 signUp.addEventListener("click", function () {
-    signUp.innerHTML = "<img class=\"rotate\" src=\"./assets/imgs/loading.png\" alt=\"loading\">";
+  signUp.innerHTML =
+    '<img class="rotate" src="./assets/imgs/loading.png" alt="loading">';
+  signUp.toggleAttribute("disabled");
+  // Validate unique email
+  validateUniqueEmail();
+  setTimeout(function () {
+    var _a, _b, _c, _d;
+    var allValid =
+      validationStates.name &&
+      validationStates.password &&
+      validationStates.phone &&
+      validationStates.email &&
+      isUnique;
+    if (allValid) {
+      popUp.style.top = "90%";
+      popUp.innerHTML = "<p>Signed Up Successfully \u2714</p>";
+      var newUser = {
+        email: userEmail.value,
+        name: userName.value,
+        password: userPassword.value,
+        phone: userPhone.value,
+      };
+      usersData.push(newUser);
+      saveToLocalStorage();
+      userEmail.value = "";
+      userName.value = "";
+      userPassword.value = "";
+      userPhone.value = "";
+      setTimeout(function () {
+        popUp.style.top = "140%";
+        window.location.href = "/Login-Task/login.html";
+      }, 2000);
+    } else {
+      popUp.style.top = "90%";
+      popUp.innerHTML = "<p>Failed to sign up \u2716</p>";
+      validationStates.name == true
+        ? null
+        : (_a = userName.parentElement) === null || _a === void 0
+        ? void 0
+        : _a.classList.add("wrong-input");
+      validationStates.email == true
+        ? null
+        : (_b = userEmail.parentElement) === null || _b === void 0
+        ? void 0
+        : _b.classList.add("wrong-input");
+      validationStates.password == true
+        ? null
+        : (_c = userPassword.parentElement) === null || _c === void 0
+        ? void 0
+        : _c.classList.add("wrong-input");
+      validationStates.phone == true
+        ? null
+        : (_d = userPhone.parentElement) === null || _d === void 0
+        ? void 0
+        : _d.classList.add("wrong-input");
+      setTimeout(function () {
+        popUp.style.top = "140%";
+      }, 2000);
+    }
+    signUp.innerHTML = "Sign Up";
     signUp.toggleAttribute("disabled");
-    // Validate unique email
-    validateUniqueEmail();
-    setTimeout(function () {
-        var _a, _b, _c, _d;
-        var allValid = validationStates.name &&
-            validationStates.password &&
-            validationStates.phone &&
-            validationStates.email &&
-            isUnique;
-        if (allValid) {
-            popUp.style.top = "90%";
-            popUp.innerHTML = "<p>Signed Up Successfully \u2714</p>";
-            var newUser = {
-                email: userEmail.value,
-                name: userName.value,
-                password: userPassword.value,
-                phone: userPhone.value,
-            };
-            usersData.push(newUser);
-            saveToLocalStorage();
-            userEmail.value = "";
-            userName.value = "";
-            userPassword.value = "";
-            userPhone.value = "";
-            setTimeout(function () {
-                popUp.style.top = "140%";
-                window.location.href = "../login.html";
-            }, 2000);
-        }
-        else {
-            popUp.style.top = "90%";
-            popUp.innerHTML = "<p>Failed to sign up \u2716</p>";
-            validationStates.name == true
-                ? null
-                : (_a = userName.parentElement) === null || _a === void 0 ? void 0 : _a.classList.add("wrong-input");
-            validationStates.email == true
-                ? null
-                : (_b = userEmail.parentElement) === null || _b === void 0 ? void 0 : _b.classList.add("wrong-input");
-            validationStates.password == true
-                ? null
-                : (_c = userPassword.parentElement) === null || _c === void 0 ? void 0 : _c.classList.add("wrong-input");
-            validationStates.phone == true
-                ? null
-                : (_d = userPhone.parentElement) === null || _d === void 0 ? void 0 : _d.classList.add("wrong-input");
-            setTimeout(function () {
-                popUp.style.top = "140%";
-            }, 2000);
-        }
-        signUp.innerHTML = "Sign Up";
-        signUp.toggleAttribute("disabled");
-    }, 1500);
+  }, 1500);
 });
 // Data fetching and saving
 (function loadSaved() {
-    document.addEventListener("DOMContentLoaded", function () {
-        var storedItems = localStorage.getItem("usersData");
-        if (storedItems) {
-            usersData = JSON.parse(storedItems);
-        }
-    });
+  document.addEventListener("DOMContentLoaded", function () {
+    var storedItems = localStorage.getItem("usersData");
+    if (storedItems) {
+      usersData = JSON.parse(storedItems);
+    }
+  });
 })();
 function saveToLocalStorage() {
-    localStorage.setItem("usersData", JSON.stringify(usersData));
+  localStorage.setItem("usersData", JSON.stringify(usersData));
 }
--- script/login.js
+++ script/login.js
@@ -4,37 +4,36 @@ var logIn = document.getElementById("logIn");
 var popUp = document.querySelector(".popup");
 var usersData = [];
 (function loadSaved() {
-    document.addEventListener("DOMContentLoaded", function () {
-        var storedItems = localStorage.getItem("usersData");
-        if (storedItems) {
-            usersData = JSON.parse(storedItems);
-        }
-    });
+  document.addEventListener("DOMContentLoaded", function () {
+    var storedItems = localStorage.getItem("usersData");
+    if (storedItems) {
+      usersData = JSON.parse(storedItems);
+    }
+  });
 })();
 // Validate login cerdintials
 logIn.addEventListener("click", validateLogIn);
 function validateLogIn() {
-    var correctLogin = { isOk: false };
-    usersData.forEach(function (e) {
-        if (loginEmail.value === e.email && loginPassword.value === e.password) {
-            correctLogin.isOk = true;
-            localStorage.setItem("loggedInUser", e.name);
-        }
-    });
-    if (correctLogin.isOk == true) {
-        popUp.innerHTML = "<p>Welcome back \u2714</p>";
-        popUp.style.top = "90%";
-        setTimeout(function () {
-            popUp.style.top = "140%";
-            window.location.href = "../comingsoon.html";
-        }, 2000);
+  var correctLogin = { isOk: false };
+  usersData.forEach(function (e) {
+    if (loginEmail.value === e.email && loginPassword.value === e.password) {
+      correctLogin.isOk = true;
+      localStorage.setItem("loggedInUser", e.name);
     }
-    else {
-        popUp.innerHTML = "<p>Email or Password isn't correct \u2716</p>";
-        popUp.style.top = "90%";
-        setTimeout(function () {
-            popUp.style.top = "140%";
-        }, 2000);
-    }
+  });
+  if (correctLogin.isOk == true) {
+    popUp.innerHTML = "<p>Welcome back \u2714</p>";
+    popUp.style.top = "90%";
+    setTimeout(function () {
+      popUp.style.top = "140%";
+      window.location.href = "/Login-Task/comingsoon.html";
+    }, 2000);
+  } else {
+    popUp.innerHTML = "<p>Email or Password isn't correct \u2716</p>";
+    popUp.style.top = "90%";
+    setTimeout(function () {
+      popUp.style.top = "140%";
+    }, 2000);
+  }
 }
 // Show popup with content
